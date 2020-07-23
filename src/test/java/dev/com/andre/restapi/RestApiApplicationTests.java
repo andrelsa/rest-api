e4f87963ee143ec36195ddf26c009383cb1383de
André Soares <andrekd3@gmail.com>
2020-07-23 20:37:18 -0300

    my first commit

---
+++ src/test/java/dev/com/andre/restapi/RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.com.andre.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
